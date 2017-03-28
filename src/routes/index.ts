3caa485e9e36f8615357bab5c162f74c10c85d9e
24wings <2121718893@qq.com>
2017-03-28 12:44:49 +0800

    主页轮播完成

---
+++ src/routes/index.ts
@@ -0,0 +1,34 @@
+import express = require('express');
+import moment = require('moment');
+
+import { topicModel, ITopic, IUser, userModel, recordModel, IRecord } from '../models';
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', async function (req, res, next) {
+  /*
+  if (res.locals.user) {
+    var user = await userModel.findById(res.locals.user._id).exec();
+    var record = await recordModel.findOne({ user: user._id }).exec();
+    console.log(user, record);
+    res.render('index', {
+    user
+    });
+  } else {
+    res.redirect('/signup');
+  }
+ */
+  res.render('index');
+
+
+});
+
+export { router as indexRouter };
+export { signinRouter } from './signin';
+export { signupRouter } from './signup';
+export { studentIndexRouter } from './student/index';
+export { signoutRouter } from './signout';
+export { createRouter } from './create';
+export { topicRouter } from './topic';
+
